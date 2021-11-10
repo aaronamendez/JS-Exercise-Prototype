a21ed22852ddf2a97c22d783c8b7924ae50f7b4d
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-11-10 15:27:03 -0500

    Finished Task 1

--- index.js
+++ index.js
@@ -9,16 +9,15 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+	this.name = name;
+	this.isFlying = false;
 }
 Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+	this.isFlying = true;
 };
 Airplane.prototype.land = function () {
-  this.isFlying = false;
+	this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,15 +38,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
+	// return stomach;
 }
 
-
-
-
-
-
+Person.prototype.eat = function (someFood) {
+	if (this.stomach.length <= 9) {
+		this.stomach.push(someFood);
+	}
+};
+Person.prototype.poop = function () {
+	this.stomach.length = 0;
+};
+Person.prototype.toString = function () {
+	return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -63,10 +71,7 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
-}
-
+function Car() {}
 
 /*
   TASK 3
@@ -75,10 +80,7 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
-}
-
+function Baby() {}
 
 /* 
   TASK 4
@@ -89,18 +91,17 @@ function Baby() {
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working!');
-  return 'bar';
+function foo() {
+	console.log("its working!");
+	return "bar";
 }
 foo();
 module.exports = {
-  foo,
-  Person, 
-  Car,
-  Baby
-}
+	foo,
+	Person,
+	Car,
+	Baby,
+};
